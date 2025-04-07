1155b787e6fab2039160751270ae7f8dbd38106c
spencercdz <spencerchudz@gmail.com>
2025-04-07 15:03:55 +0800

    Added script to handle collection of raw data and metadata

---
+++ src/data/data_loader.py
@@ -0,0 +1,36 @@
+# data_loader.py: Data Loader Utilities
+import os
+import json
+from datasets import load_dataset
+from huggingface_hub import login
+
+# Authenticate to HuggingFace
+login(token="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")  # Replace with your token
+
+# Create target path
+current_dir = os.path.dirname(os.path.abspath(__file__))  # src/data/
+project_root = os.path.dirname(os.path.dirname(current_dir))  # Goes up to project/
+raw_path = os.path.join(project_root, 'data', 'raw')  # project/data/raw/
+metadata_path = os.path.join(project_root, 'data', 'metadata') # project/data/metadata/
+
+# Load Hugging Face dataset (Natural Disasters) in data/raw
+ds = load_dataset(
+    "melisekm/natural-disasters-from-social-media",
+    )
+
+# Save dataset to my folder
+for split in ["train", "test", "validation"]:
+    file_path = os.path.join(raw_path, f"{split}.csv")
+    ds[split].to_csv(file_path, index=False)
+    print(f"Saved {split} to: {file_path}")  # Verification
+
+# Save metadata
+metadata = {
+    "dataset_name": "natural-disasters-from-social-media",
+    "description": ds['train'].info.description,
+    "features": str(ds["train"].features),
+    "num_samples": {split: len(ds[split]) for split in ds.keys()}
+}
+
+with open(os.path.join(metadata_path, "dataset_metadata.json"), "w") as f:
+    json.dump(metadata, f, indent=2)
